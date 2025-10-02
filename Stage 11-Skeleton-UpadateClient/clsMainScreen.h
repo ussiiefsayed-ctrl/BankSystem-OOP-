88f92df37a3b18c984b4baf6aea4c8f39912fd76
ussiiefsayed-ctrl <ussiiefsayed@gmail.com>
2025-10-02 03:16:04 +0300

    Update clsMainScreen.h

--- Stage 11-Skeleton-UpadateClient/clsMainScreen.h
+++ Stage 11-Skeleton-UpadateClient/clsMainScreen.h
@@ -6,6 +6,7 @@
 #include "clsClientListScreen.h"
 #include "clsAddNewClientScreen.h"
 #include "clsDeleteClientScreen.h"
+#include "clsUpdateClientScreen.h"
 
 using namespace std;
 
@@ -55,8 +56,8 @@ private:
 
     static void _ShowUpdateClientScreen()
     {
-        cout << "\nUpdate Client Screen Will be here...\n";
-
+        //cout << "\nUpdate Client Screen Will be here...\n";
+        clsUpdateClientScreen::ShowUpdateClientScreen();
     }
 
     static void _ShowFindClientScreen()
@@ -166,5 +167,6 @@ public:
     }
 
 };
+
 
 
